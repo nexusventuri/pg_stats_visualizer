3c39c4fd0a972d52c0d3521480f0de45a005d0ab
Ken Alex Fassone <nexusventuri@gmail.com>
2019-04-28 18:24:41 +0100

    Remove comments

--- app/javascript/components/data_visualization/HistogramChart.js
+++ app/javascript/components/data_visualization/HistogramChart.js
@@ -72,14 +72,7 @@ export default class HistogramChart extends Component {
   }
 
   render () {
-    //var quantize = scaleQuantize(this.histogramBounds, [this.histogramBounds[0], this.histogramBounds[this.histogramBounds.length - 1]]);
-    //console.log([this.histogramBounds[0], this.histogramBounds[this.histogramBounds.length - 1]])
-    //var quantile = scaleQuantile(this.histogramBounds, this.histogramBounds.map((x, i) => { return i; }));
-    //console.log('data', this.data);
-    //console.log('quantile', this.histogramBounds, this.histogramBounds.map((x, i) => { return i == 0 ? 0:1; }), quantile.quantiles())
     var linear = scaleLinear(this.histogramBounds, [0, 100]);
-    //var threshold = scaleThreshold([0,100], this.histogramBounds);
-    //console.log(this.data);
     return (
       <div key='play-around'>
         <LineChart width={800} height={300} data={this.data} margin={{top: 5, right: 30, left: 20, bottom: 5}} >
@@ -91,19 +84,5 @@ export default class HistogramChart extends Component {
         </LineChart>
       </div>
     );
-        //<LineChart width={800} height={300} data={this.data} margin={{top: 5, right: 30, left: 20, bottom: 5}} >
-        //  <XAxis dataKey="name" scale={quantize} />
-        //  <YAxis/>
-        //  <Tooltip/>
-        //  <Legend />
-        //  <Line type="monotone" dataKey="percentage" stroke="#8884d8"/>
-        //</LineChart>
-        //<LineChart width={800} height={300} data={this.data} margin={{top: 5, right: 30, left: 20, bottom: 5}} >
-        //  <XAxis dataKey="name" scale={threshold}/>
-        //  <YAxis/>
-        //  <Tooltip/>
-        //  <Legend />
-        //  <Line type="monotone" dataKey="percentage" stroke="#8884d8"/>
-        //</LineChart>
   }
 }
